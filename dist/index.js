8af0c5fa15cd2ae5a08f507414e778361c016488
aykutkardas <aykutkrds@gmail.com>
2018-12-31 17:48:21 +0300

    Module index added.

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+import Limit from "./limit";
+import Select from "./select";
+import Where from "./where";
+import OrderBy from "./orderBy";
+import ActiveRecord from "./active-record";
+export { Limit, Select, Where, OrderBy };
+export default ActiveRecord;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import Limit from "./limit";
+import Select from "./select";
+import Where from "./where";
+import OrderBy from "./orderBy";
+import ActiveRecord from "./active-record";
+
+export { Limit, Select, Where, OrderBy };
+
+export default ActiveRecord;
