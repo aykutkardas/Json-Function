5b604dea45044842f4690802481a54aeca6bf42e
Aykut Kardaş <aykutkrds@gmail.com>
2019-01-13 17:05:58 +0300

    Add new type-check (isArrayOfString, isArrayOfObject, isObject, isOneOf)

--- src/type-check/index.ts
+++ src/type-check/index.ts
@@ -1,15 +1,41 @@
 type TypeCheckFunction = (value: any) => boolean;
 type OneOfCheckFunction = (value: any, options: any[]) => boolean;
+type GetTypeFunction = (value: any) => string;
+
+const getType: GetTypeFunction = value => toString.call(value);
 
 const isNumber: TypeCheckFunction = value =>
   value && typeof value === "number" && !isNaN(value);
 
 const isArray: TypeCheckFunction = value => value && Array.isArray(value);
 
+const isArrayOfString: TypeCheckFunction = value =>
+  value &&
+  isArray(value) &&
+  !value.map((val: any) => isString(val)).includes(false);
+
+const isArrayOfObject: TypeCheckFunction = value =>
+  value &&
+  isArray(value) &&
+  !value.map((val: any) => isObject(val)).includes(false);
+
+const isObject: TypeCheckFunction = value =>
+  value && getType(value) === "[object Object]";
+
 const isString: TypeCheckFunction = value => value && typeof value === "string";
 
 const isDefined: TypeCheckFunction = value => value !== undefined;
 
-const isOneOf: OneOfCheckFunction = (value, options) => isArray(options) ? options.includes(value) : false;
+const isOneOf: OneOfCheckFunction = (value, options) =>
+  isArray(options) ? options.includes(value) : false;
 
-export { isNumber, isArray, isString, isDefined, isOneOf };
+export {
+  isNumber,
+  isArray,
+  isArrayOfString,
+  isArrayOfObject,
+  isString,
+  isDefined,
+  isOneOf,
+  isObject
+};
