47b43ff4cfed033d4d5d920c054f3673c28102b4
Aykut Kardaş <aykutkrds@gmail.com>
2019-01-13 17:20:46 +0300

    Where method type-check added and code refactor.

--- src/where.ts
+++ src/where.ts
@@ -1,25 +1,30 @@
+import { isArray, isObject, isArrayOfObject } from "./type-check";
+
 type WhereFunction = (data: Object[], queries: Object | Object[]) => Object[];
 
-const where: WhereFunction = (data = [], queries = []) => {
+const where: WhereFunction = (data, queries) => {
+  if (!isArray(data)) {
+    return [];
+  }
+
   let queriesArr: Object[];
+
+  if (isObject(queries)) {
+    queriesArr = [queries];
+  } else if (isArrayOfObject(queries)) {
+    queriesArr = <Object[]>queries;
+  } else {
+    return data;
+  }
+
   let matchingItems: Object[] = [];
   let result: Object[] = [];
-  let indexes = [];
-
-  if (!Array.isArray(queries)) {
-    queriesArr = [queries];
-  } else {
-    queriesArr = queries;
-  }
 
   queriesArr.forEach(query => {
     Object.keys(query).forEach(fieldName => {
       matchingItems = data.filter((item, index) => {
         if (item[fieldName] === query[fieldName]) {
-          if (indexes.indexOf(index) === -1) {
-            indexes.push(index);
-            return true;
-          }
+          return true;
         }
       });
     });
