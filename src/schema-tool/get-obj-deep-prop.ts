591f7e9dab4f00e4b9195ef54e3cda061f79eece
aykutkardas <aykutkrds@gmail.com>
2019-01-25 16:45:22 +0300

    get-obj-deep-prop update.

--- src/schema-tool/get-obj-deep-prop.ts
+++ src/schema-tool/get-obj-deep-prop.ts
@@ -1,20 +1,32 @@
-type GetObjDeepPropFunction = (obj: Object, props: string, defaultValue?: any) => any;
+type GetObjDeepPropFunction = (
+  obj: Object,
+  props: string,
+  defaultValue?: any
+) => any;
 
 const getObjDeepProp: GetObjDeepPropFunction = (obj, props, defaultValue) => {
   if (!obj) return false;
-  if (typeof obj !== 'object') return false;
+  if (typeof obj !== "object") return false;
   if (!props) return obj;
-  if (typeof props !== 'string') return false;
+  if (typeof props !== "string") return false;
 
-  const propsArr = props.split('.');
+  const propsArr = props.split(".");
   let rootObj: any = obj;
 
-  propsArr.forEach((prop) => {
-    if (typeof rootObj[prop] !== 'undefined' || rootObj[prop] !== null || !isNaN(rootObj[prop])) {
-      rootObj = rootObj[prop];
-    } else {
+  propsArr.forEach(prop => {
+    if (!rootObj) {
       rootObj = false;
+      return;
     }
+    if (
+      typeof rootObj[prop] !== "undefined" ||
+      rootObj[prop] !== null ||
+      !isNaN(rootObj[prop])
+    ) {
+      rootObj = rootObj[prop];
+      return;
+    }
+    rootObj = false;
   });
 
   return rootObj !== false ? rootObj : defaultValue || false;
