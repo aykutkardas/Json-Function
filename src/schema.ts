43aed614e99a0b85ad93a19d438c98dc1badd3a0
Aykut Kardaş <aykut@90pixel.com>
2019-03-07 18:12:32 +0300

    Schema object update.

--- src/schema.ts
+++ src/schema.ts
@@ -13,8 +13,17 @@ const schema: SchemaFunction = (data = [], schema = {}) => {
   const result: Object[] = [];
   const fields: string[] = [];
 
-  data.forEach((item) => {
+  data.forEach(item => {
     const temp = JSON.parse(JSON.stringify(schemaObj));
+
+    Object.keys(schemaObj).forEach(key => {
+      if (!schemaObj[key].__schema__) {
+        return false;
+      }
+      Object.keys(schemaObj[key].__schema__).forEach(subKey => {
+        temp[key].__schema__[subKey] = schemaObj[key].__schema__[subKey];
+      });
+    });
     result.push(getSchemaValue(temp, item, fields));
   });
 
