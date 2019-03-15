f82dd82292a6eaaa47ed5810aea692424f2f3976
Aykut Kardaş <aykutkrds@gmail.com>
2019-03-15 22:18:46 +0300

    Where Function methods test added.

--- test/where.spec.ts
+++ test/where.spec.ts
@@ -109,4 +109,181 @@ describe("Where Tools Function", () => {
       }
     ]);
   });
+  it(".gte() method.", () => {
+    const result = where(data, wh => ({
+      id: wh.gte(3)
+    }));
+    expect(result).to.deep.equal([
+      {
+        userId: 2,
+        id: 3,
+        title: "fugiat veniam minus",
+        completed: false,
+        education: {
+          isDone: false
+        }
+      },
+      {
+        userId: 1,
+        id: 4,
+        title: "et porro tempora",
+        completed: true
+      }
+    ]);
+  });
+  it(".lt() method.", () => {
+    const result = where(data, wh => ({
+      id: wh.lt(3)
+    }));
+    expect(result).to.deep.equal([
+      {
+        userId: 1,
+        id: 1,
+        title: "delectus aut autem",
+        completed: false,
+        education: {
+          isDone: true
+        }
+      },
+      {
+        userId: 2,
+        id: 2,
+        title: "quis ut nam facilis et officia qui",
+        completed: false,
+        education: {
+          isDone: false
+        }
+      }
+    ]);
+  });
+  it(".lte() method.", () => {
+    const result = where(data, wh => ({
+      id: wh.lte(3)
+    }));
+    expect(result).to.deep.equal([
+      {
+        userId: 1,
+        id: 1,
+        title: "delectus aut autem",
+        completed: false,
+        education: {
+          isDone: true
+        }
+      },
+      {
+        userId: 2,
+        id: 2,
+        title: "quis ut nam facilis et officia qui",
+        completed: false,
+        education: {
+          isDone: false
+        }
+      },
+      {
+        userId: 2,
+        id: 3,
+        title: "fugiat veniam minus",
+        completed: false,
+        education: {
+          isDone: false
+        }
+      }
+    ]);
+  });
+  it(".eq() method.", () => {
+    const result = where(data, wh => ({
+      id: wh.eq("3")
+    }));
+    expect(result).to.deep.equal([
+      {
+        userId: 2,
+        id: 3,
+        title: "fugiat veniam minus",
+        completed: false,
+        education: {
+          isDone: false
+        }
+      }
+    ]);
+  });
+  it(".ne() method.", () => {
+    const result = where(data, wh => ({
+      id: wh.ne("3")
+    }));
+    expect(result).to.deep.equal([
+      {
+        userId: 1,
+        id: 1,
+        title: "delectus aut autem",
+        completed: false,
+        education: {
+          isDone: true
+        }
+      },
+      {
+        userId: 2,
+        id: 2,
+        title: "quis ut nam facilis et officia qui",
+        completed: false,
+        education: {
+          isDone: false
+        }
+      },
+      {
+        userId: 1,
+        id: 4,
+        title: "et porro tempora",
+        completed: true
+      }
+    ]);
+  });
+  it(".in() method.", () => {
+    const result = where(data, wh => ({
+      title: wh.in("nam")
+    }));
+    expect(result).to.deep.equal([
+      {
+        userId: 2,
+        id: 2,
+        title: "quis ut nam facilis et officia qui",
+        completed: false,
+        education: {
+          isDone: false
+        }
+      }
+    ]);
+    
+  });
+  it(".nin() method.", () => {
+    const result = where(data, wh => ({
+      title: wh.nin("nam")
+    }));
+    expect(result).to.deep.equal([
+      {
+        userId: 1,
+        id: 1,
+        title: "delectus aut autem",
+        completed: false,
+        education: {
+          isDone: true
+        }
+      },
+      {
+        userId: 2,
+        id: 3,
+        title: "fugiat veniam minus",
+        completed: false,
+        education: {
+          isDone: false
+        }
+      },
+      {
+        userId: 1,
+        id: 4,
+        title: "et porro tempora",
+        completed: true
+      }
+    ]);
+    
+  });
 });
