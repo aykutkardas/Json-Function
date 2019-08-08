6711b88a114308062b63647b1b7744b4d43d7128
Aykut Kardaş <aykutkrds@gmail.com>
2019-08-09 00:26:08 +0300

    TypeCheck methods problem fixed.

--- src/type-check/index.ts
+++ src/type-check/index.ts
@@ -4,27 +4,23 @@ type GetTypeFunction = (value: any) => string;
 
 const getType: GetTypeFunction = value => toString.call(value);
 
-const isNumber: TypeCheckFunction = value =>
-  value && typeof value === "number" && !isNaN(value);
+const isDefined: TypeCheckFunction = value => value !== undefined;
+
+const isNumber: TypeCheckFunction = value => typeof value === "number" && !isNaN(value);
+
+const isString: TypeCheckFunction = value => typeof value === "string";
 
 const isArray: TypeCheckFunction = value => value && Array.isArray(value);
 
-const isArrayOfString: TypeCheckFunction = value =>
-  value &&
-  isArray(value) &&
+const isArrayOfString: TypeCheckFunction = value => isArray(value) && 
   !value.map((val: any) => isString(val)).includes(false);
 
-const isArrayOfObject: TypeCheckFunction = value =>
-  value &&
-  isArray(value) &&
+const isArrayOfObject: TypeCheckFunction = value => isArray(value) &&
   !value.map((val: any) => isObject(val)).includes(false);
 
 const isObject: TypeCheckFunction = value =>
   value && getType(value) === "[object Object]";
 
-const isString: TypeCheckFunction = value => value && typeof value === "string";
-
-const isDefined: TypeCheckFunction = value => value !== undefined;
 
 const isOneOf: OneOfCheckFunction = (value, options) =>
   isArray(options) ? options.includes(value) : false;
