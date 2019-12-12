e599588c878d961431aa0a533850fa65011aa852
Aykut Kardaş <aykutkrds@gmail.com>
2019-12-12 14:10:37 +0300

    Transform function processVal method fixed.

--- src/transform.ts
+++ src/transform.ts
@@ -17,7 +17,7 @@ const transformKeys = (obj: Object): Object => {
 }
 
 const processVal = (val: any): any => {
-  if(typeof(val) !== 'object' ) {
+  if(!val || typeof(val) !== 'object' ) {
     return val;
   } else if (isArray(val)) {
     return val.map(transformKeys);
