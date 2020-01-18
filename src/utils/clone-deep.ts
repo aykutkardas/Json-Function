e9243863fb1a1ff9510a5a5fa324044ec5ed6242
Aykut Kardaş <aykutkrds@gmail.com>
2020-01-18 11:56:49 +0300

    CloneDeep utils refactor.

--- src/utils/clone-deep.ts
+++ src/utils/clone-deep.ts
@@ -1,18 +1,28 @@
 // https://stackoverflow.com/questions/122102/what-is-the-most-efficient-way-to-deep-clone-an-object-in-javascript
-function cloneDeep(obj) {
-  if (obj === null || typeof obj !== "object" || "isActiveClone" in obj)
-    return obj;
 
-  var temp = obj.constructor();
+import { isNull, isObject } from './type-check';
 
-  for (var key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      obj["isActiveClone"] = null;
-      temp[key] = cloneDeep(obj[key]);
-      delete obj["isActiveClone"];
-    }
+export default function cloneDeep(object) {
+  if (
+    isNull(object) 
+    || !isObject(object) 
+    || "isActiveClone" in object
+  ) {
+    return object;
   }
-  return temp;
-}
 
-export default cloneDeep;
+  const newObject: Object = object.constructor();
+
+  for (const key in object) {
+
+    if (!Object.prototype.hasOwnProperty.call(object, key)) {
+      continue;
+    }
+
+    object.isActiveClone = null;
+    newObject[key] = cloneDeep(object[key]);
+    delete object.isActiveClone;
+  }
+
+  return newObject;
+}
