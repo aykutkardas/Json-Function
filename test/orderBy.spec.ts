3ec4fdddb6c9efeac00f873b89e0b33f4ee12441
Aykut Kardaş <aykutkrds@gmail.com>
2020-05-09 21:13:04 +0300

    More test added for orderBy.

--- test/orderBy.spec.ts
+++ test/orderBy.spec.ts
@@ -34,7 +34,7 @@ const data = [
 ];
 
 describe("OrdeyBy Function", () => {
-  it("Data sorting test using OrderBy function.", () => {
+  it("Data 'default' sorting test using OrderBy function.", () => {
     const result = orderBy(data, "title");
     expect(result).to.deep.equal([
       {
@@ -68,7 +68,75 @@ describe("OrdeyBy Function", () => {
     ]);
   });
 
-  it("Data sorting test with nested key using OrderBy function.", () => {
+  it("Data 'ASC' sorting test using OrderBy function.", () => {
+    const result = orderBy(data, "title", "ASC");
+    expect(result).to.deep.equal([
+      {
+        userId: 1,
+        id: 1,
+        title: "delectus aut autem",
+        completed: false,
+        meta: { value: "a" },
+      },
+      {
+        userId: 1,
+        id: 4,
+        title: "et porro tempora",
+        completed: true,
+        meta: { value: "d" },
+      },
+      {
+        userId: 1,
+        id: 3,
+        title: "fugiat veniam minus",
+        completed: false,
+        meta: { value: "c" },
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: "quis ut nam facilis et officia qui",
+        completed: false,
+        meta: { value: "b" },
+      },
+    ]);
+  });
+
+  it("Data 'DESC' sorting test using OrderBy function.", () => {
+    const result = orderBy(data, "title", "DESC");
+    expect(result).to.deep.equal([
+      {
+        userId: 1,
+        id: 2,
+        title: "quis ut nam facilis et officia qui",
+        completed: false,
+        meta: { value: "b" },
+      },
+      {
+        userId: 1,
+        id: 3,
+        title: "fugiat veniam minus",
+        completed: false,
+        meta: { value: "c" },
+      },
+      {
+        userId: 1,
+        id: 4,
+        title: "et porro tempora",
+        completed: true,
+        meta: { value: "d" },
+      },
+      {
+        userId: 1,
+        id: 1,
+        title: "delectus aut autem",
+        completed: false,
+        meta: { value: "a" },
+      },
+    ]);
+  });
+
+  it("Data 'DESC' sorting test with nested key using OrderBy function.", () => {
     const result = orderBy(data, "meta.value", "DESC", { deep: true });
     expect(result).to.deep.equal([
       {
@@ -98,6 +166,40 @@ describe("OrdeyBy Function", () => {
         title: "delectus aut autem",
         completed: false,
         meta: { value: "a" },
+      },
+    ]);
+  });
+
+  it("Data 'ASC' sorting test with nested key using OrderBy function.", () => {
+    const result = orderBy(data, "meta.value", "ASC", { deep: true });
+    expect(result).to.deep.equal([
+      {
+        userId: 1,
+        id: 1,
+        title: "delectus aut autem",
+        completed: false,
+        meta: { value: "a" },
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: "quis ut nam facilis et officia qui",
+        completed: false,
+        meta: { value: "b" },
+      },
+      {
+        userId: 1,
+        id: 3,
+        title: "fugiat veniam minus",
+        completed: false,
+        meta: { value: "c" },
+      },
+      {
+        userId: 1,
+        id: 4,
+        title: "et porro tempora",
+        completed: true,
+        meta: { value: "d" },
       },
     ]);
   });
